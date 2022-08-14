cbc2727be820c66826dc39500e800e210c155eed
bondspear <71352526+bondspear@users.noreply.github.com>
2022-08-14 23:11:15 +0300

    Update Knockout require-config.js

--- Knockout require-config.js
+++ Knockout require-config.js
@@ -1,7 +1,8 @@
 var config = {
         map: {
             '*': {
-                'VendorName_ModuleName/js/base_component': 'VendorName_ModuleName/js/custom_component'
+                'VendorName_ModuleName/js/base_component': 'VendorName_ModuleName/js/custom_component',
+                'VendorName_ModuleName/base_template.html': 'VendorName_ModuleName/custom_template.html'
             }
         },
         config: {
