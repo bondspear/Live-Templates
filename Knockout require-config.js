c9ace56d65f11810baba84f914a1f8ca0a38f5cf
bondspear <71352526+bondspear@users.noreply.github.com>
2022-08-14 23:28:48 +0300

    Update Knockout require-config.js

--- Knockout require-config.js
+++ Knockout require-config.js
@@ -1,9 +1,6 @@
 /**
  *
- * @description: after any changes!!
- *
- * 1) delete:  require-config.js  (into pub/static)
- * 2) use:     php bin/magento c:f && php bin/magento s:s:d -f (locale)
+ * @description: php bin/magento c:f && php bin/magento s:s:d -f (locale)
  */
 
 var config = {
